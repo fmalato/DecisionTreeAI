13c71e80a02bc972ae68d7fdbd872b3fdec2e356
Federico Màlato <federico.malato@gmail.com>
2018-03-16 15:55:33 +0100

    Initial commit to synchronize both notebook and home PC

---
+++ DecisionTree.py
@@ -0,0 +1,20 @@
+import pandas as pnd
+from sklearn import tree
+from sklearn import datasets
+from sklearn.datasets import load_iris
+import graphviz
+
+def decisionTreeLearning(examples, attributes, parentExamples):
+    if examples is empty:
+        return pluralityValue(parentExamples)
+    elif all examples have the same classification:
+        return classification
+    elif attributes is empty:
+        return pluralityValue(examples)
+    else:
+        A = argmax(importance(a, examples))
+        tree =
+    return tree
+
+def pluralityValue(set):
+    return set
